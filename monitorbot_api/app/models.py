b1c3e55a5b5010566fd9e70b9106602f5a4296df
Awili Uzo <awiliuzo@gmail.com>
2015-06-17 10:33:59 +0100

    user and watch models update

--- monitorbot_api/app/models.py
+++ monitorbot_api/app/models.py
@@ -1,5 +1,12 @@
-from . import db
+
+from flask import current_app, request, url_for
 from datetime import datetime
+from . import db
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# from flask.ext.login import UserMixin
 
 
 class Role(db.Model):
@@ -13,9 +20,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-
 class Frequency(db.Model):
+    
     __tablename__ = 'frequencies'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     value = db.Column(db.BigInteger, unique=True)
@@ -26,17 +34,61 @@ class Frequency(db.Model):
         return '<Role %r>' % self.name
 
 
-
 class User(db.Model):
+
     __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password = db.Column(db.String(64), index=True)
+    password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    watchs = db.relationship('Watch', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+   
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def to_json(self):
+        json_user = {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'role': self.role,
+            'watch_count': self.watchs.all().count()
+        }
+        return json_user
+
+    # @staticmethod
+    # def from_json(json_rep):
+    #     body = json_rep.get('body')
+    #     if body is None or body == '':
+    #         raise ValidationError('post does not have a body')
+    #     return Post(body=body)
 
 
 class Watch(db.Model):
@@ -44,6 +96,7 @@ class Watch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(64))
     frequency_id = db.Column(db.Integer, db.ForeignKey('frequencies.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     email = db.Column(db.String(64))
     client = db.Column(db.String(64))
     timestamp = db.Column(db.BigInteger)
@@ -67,15 +120,5 @@ class Check(db.Model):
         return '<Check %r>' % datetime.fromtimestamp(self.timestamp)
 
 
-# class Client(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), unique=True, index=True)
-#     password = db.Column(db.String(64), index=True)
-#     email = db.Column(db.String(64), unique=True, index=True)
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-# 
-#     def __repr__(self):
-#         return '<User %r>' % self.username
 
 
