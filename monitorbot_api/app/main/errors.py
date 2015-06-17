7d97608c51c25974b9f9d820669041690b534be8
Awili Uzo <awiliuzo@gmail.com>
2015-06-17 10:40:16 +0100

    monitorbot_api_project_scaffold

---
+++ monitorbot_api/app/main/errors.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from . import main
+
+
+@main.app_errorhandler(404)
+def resource_not_found(e):
+    return 'IMPLEMENT-JSON-FORMATED-ERROR-RESPONSE-HERE', 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return 'IMPLEMENT-JSON-FORMATED-ERROR-RESPONSE-HERE', 500
