e674cf7a632e90c532d5c234f945c6ab26765130
IniOluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2015-06-18 20:09:08 +0100

    latest changes

--- monitorbot_api/config.py
+++ monitorbot_api/config.py
@@ -14,6 +14,14 @@ class Config:
     MONITOR_BOT_MAIL_SENDER = 'Monitor Bot <monitorbot.app@gmail.com>'
     MONITOR_BOT_ADMIN = os.environ.get('MB_ADMIN') or '<monitorbot.app@gmail.com>'
 
+    BROKER_URL = 'localhost:6379'
+    # redis://:Cloudbot@hostname:port/db_number
+    BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}  # 1 hour.
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    BROKER_TRANSPORT_OPTIONS = {'fanout_prefix': True}
+    BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 43200}
+
+
     @staticmethod
     def init_app(app):
         pass
@@ -44,9 +52,9 @@ config = {
     'default': DevelopmentConfig
 }
 
-BROKER_URL = 'redis://localhost:6379/0'
+#BROKER_URL = 'redis://localhost:6379/0'
 # redis://:Cloudbot@hostname:port/db_number
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}  # 1 hour.
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-BROKER_TRANSPORT_OPTIONS = {'fanout_prefix': True}
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 43200}
+#BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}  # 1 hour.
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+#BROKER_TRANSPORT_OPTIONS = {'fanout_prefix': True}
+#BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 43200}
